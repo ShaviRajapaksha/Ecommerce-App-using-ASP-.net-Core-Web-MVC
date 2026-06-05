5eca5274f5f026e68a7e1aff8cb496a6d7b8fe19
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-06-05 21:45:49 +0530

    Ft: Created Order Product View Model

---
+++ MyEcommerceMvc/Models/ViewModels/OrderProductViewModel.cs
@@ -0,0 +1,11 @@
+namespace MyEcommerceMvc.Models.ViewModels;
+
+public class OrderProductViewModel
+{
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal Subtotal { get; set; }
+}
