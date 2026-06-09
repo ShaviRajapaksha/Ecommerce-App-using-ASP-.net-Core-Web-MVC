1281a550a143b9545d05d04c413618d53014f434
ShaviRajapaksha <shavindurajapaksha@gmail.com>
2026-06-09 22:13:40 +0530

    Ft: Modify App settings

--- MyEcommerceMvc/Controllers/UsersController.cs
+++ MyEcommerceMvc/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyEcommerceMvc.Data;
+using MyEcommerceMvc.Models;
+using MyEcommerceMvc.Models.ViewModels;
 
 namespace MyEcommerceMvc.Controllers;
 
@@ -13,7 +15,7 @@ public class UsersController : Controller
         _context = context;
     }
     
-    // Get: Users
+    // GET: Users
     public async Task<IActionResult> Index()
     {
         var users = await _context.Users
@@ -23,7 +25,186 @@ public class UsersController : Controller
         return View(users);
     }
     
+    // GET: Users/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+            return NotFound();
+        
+        var user = await _context.Users
+            .Include(u => u.Profile)
+            .Include(u => u.Orders)
+            .ThenInclude(o => o.OrderProducts)
+            .ThenInclude(op => op.Product)
+            .FirstOrDefaultAsync(u => u.Id == id);
+        
+        if (user == null)
+            return NotFound();
+        
+        return View(user);
+    }
     
+    // GET: Users/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
     
+    // POST: Users/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(UserProfileViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new User
+            {
+                Username = model.Username,
+                Email = model.Email,
+                CreatedAt = DateTime.UtcNow
+            };
+            
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            
+            var profile = new Profile
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Phone = model.Phone,
+                Address = model.Address,
+                UserId = user.Id
+            };
+            
+            _context.Profiles.Add(profile);
+            await _context.SaveChangesAsync();
+            
+            TempData["Success"] = "User created successfully!";
+            return RedirectToAction(nameof(Index));
+        }
+        
+        return View(model);
+    }
     
+    // GET: Users/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+            return NotFound();
+        
+        var user = await _context.Users
+            .Include(u => u.Profile)
+            .FirstOrDefaultAsync(u => u.Id == id);
+        
+        if (user == null)
+            return NotFound();
+        
+        var model = new UserProfileViewModel
+        {
+            UserId = user.Id,
+            Username = user.Username,
+            Email = user.Email,
+            FirstName = user.Profile?.FirstName ?? "",
+            LastName = user.Profile?.LastName ?? "",
+            Phone = user.Profile?.Phone,
+            Address = user.Profile?.Address
+        };
+        
+        return View(model);
+    }
+    
+    // POST: Users/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, UserProfileViewModel model)
+    {
+        if (id != model.UserId)
+            return NotFound();
+        
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                var user = await _context.Users
+                    .Include(u => u.Profile)
+                    .FirstOrDefaultAsync(u => u.Id == id);
+                
+                if (user == null)
+                    return NotFound();
+                
+                user.Username = model.Username;
+                user.Email = model.Email;
+                
+                if (user.Profile != null)
+                {
+                    user.Profile.FirstName = model.FirstName;
+                    user.Profile.LastName = model.LastName;
+                    user.Profile.Phone = model.Phone;
+                    user.Profile.Address = model.Address;
+                }
+                else
+                {
+                    var profile = new Profile
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Phone = model.Phone,
+                        Address = model.Address,
+                        UserId = user.Id
+                    };
+                    _context.Profiles.Add(profile);
+                }
+                
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "User updated successfully!";
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(model.UserId))
+                    return NotFound();
+                throw;
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+        
+        return View(model);
+    }
+    
+    // GET: Users/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+            return NotFound();
+        
+        var user = await _context.Users
+            .Include(u => u.Profile)
+            .FirstOrDefaultAsync(u => u.Id == id);
+        
+        if (user == null)
+            return NotFound();
+        
+        return View(user);
+    }
+    
+    // POST: Users/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            TempData["Success"] = "User deleted successfully!";
+        }
+        
+        return RedirectToAction(nameof(Index));
+    }
+    
+    private bool UserExists(int id)
+    {
+        return _context.Users.Any(e => e.Id == id);
+    }
 }
--- MyEcommerceMvc/Program.cs
+++ MyEcommerceMvc/Program.cs
@@ -1,7 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MyEcommerceMvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Swagger for API testing
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "E-Commerce API",
+        Version = "v1",
+        Description = "API for E-Commerce Application"
+    });
+});
 
 var app = builder.Build();
 
@@ -9,15 +28,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "E-Commerce API v1"));
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
